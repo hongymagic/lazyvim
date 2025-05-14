6291357fb146ea46ff9e44bb3bcc209adf379b98
hongymagic <hongymagic@users.noreply.github.com>
2025-05-14 21:33:22 +1000

    feat: initial commit 🎉

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, flavour = "mocha" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
